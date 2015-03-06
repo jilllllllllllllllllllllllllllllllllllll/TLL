b8d4dd8a51a6921a4dde7b371814d2f91425923a
jilllllllllllllllllllllllllllllllllllll <tkmdrhtt@gmail.com>
2015-03-07 02:56:24 +0800

    Initial
    w/ index, other links inactive.

---
+++ assets/js/foundation.mix.js
@@ -0,0 +1,22 @@
+@import '../js/foundation.js';
+
+/*
+ * You should really comment out any of the following not being used
+ * Mixture will then output a new combined and minified file
+ */
+@import '../js/foundation.alert.js';
+@import '../js/foundation.accordion.js';
+@import '../js/foundation.clearing.js';
+@import '../js/foundation.abide.js';
+@import '../js/foundation.dropdown.js';
+@import '../js/foundation.equalizer.js';
+@import '../js/foundation.interchange.js';
+@import '../js/foundation.joyride.js';
+@import '../js/foundation.magellan.js';
+@import '../js/foundation.offcanvas.js';
+@import '../js/foundation.orbit.js';
+@import '../js/foundation.reveal.js';
+@import '../js/foundation.tab.js';
+@import '../js/foundation.tooltip.js';
+@import '../js/foundation.topbar.js';
+@import '../js/foundation.slider.js';
